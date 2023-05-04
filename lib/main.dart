8e212b105404414f60b59658eaa85db287412e50
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2023-05-05 01:06:02 +0530

    material you support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quotes_app/screens/home.dart';
 //import 'package:quotes_app/theme/theme.dart';
-
+import 'package:dynamic_color/dynamic_color.dart';
 import 'screens/about.dart';
 import 'screens/customize.dart';
 import 'screens/favorites.dart';
@@ -10,26 +10,43 @@ void main() {
   runApp(const MyApp());
 }
 
+Color brandColor = Color.fromARGB(255, 52, 194, 226);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     // const appTheme = AppTheme();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Quotes For You',
-      // theme: appTheme.toThemeData(),
-      theme: ThemeData(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      //color: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => const HomePage(),
-        '/favorites': (context) => const Favorites(),
-        '/customize': (context) => Customize(),
-        '/about': (context) => const About(),
-      },
-    );
+    return DynamicColorBuilder(
+        builder: (ColorScheme? lightDynamic, ColorScheme? dark) {
+      ColorScheme lightColorScheme;
+      ColorScheme darkColorScheme;
+      if (lightDynamic != null && dark != null) {
+        lightColorScheme = lightDynamic.harmonized()..copyWith();
+        lightColorScheme = lightColorScheme.copyWith(secondary: brandColor);
+        darkColorScheme = dark.harmonized();
+      } else {
+        lightColorScheme = ColorScheme.fromSeed(seedColor: brandColor);
+        darkColorScheme =
+            ColorScheme.fromSeed(seedColor: brandColor, brightness: Brightness.dark);
+      }
+
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Quotes For You',
+        // theme: appTheme.toThemeData(),
+        theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+        darkTheme: ThemeData.dark(useMaterial3: true),
+        //color: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        initialRoute: '/home',
+        routes: {
+          '/home': (context) => const HomePage(),
+          '/favorites': (context) => const Favorites(),
+          '/customize': (context) => Customize(),
+          '/about': (context) => const About(),
+        },
+      );
+    });
   }
 }
--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -1,42 +1,88 @@
 import 'package:flutter/material.dart';
 
-class Favorites extends StatelessWidget {
+class Favorites extends StatefulWidget {
   const Favorites({super.key});
 
   @override
+  State<Favorites> createState() => _FavoritesState();
+}
+
+class _FavoritesState extends State<Favorites> {
+  @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    //     appBar: AppBar(
+    //       leading: Padding(
+    //         padding: const EdgeInsets.only(top: 14),
+    //         child: IconButton(
+    //             onPressed: () {
+    //               Navigator.of(context).pop();
+    //             },
+    //             icon: const Icon(Icons.arrow_back, size: 34,)),
+    //       ),
+    //       backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+    //       titleTextStyle: Theme.of(context).appBarTheme.titleTextStyle,
+    //     ),
+    //     body: Column(
+    //       mainAxisAlignment: MainAxisAlignment.start,
+    //       crossAxisAlignment: CrossAxisAlignment.start,
+    //       children: [
+    //         Padding(
+    //           padding: const EdgeInsets.fromLTRB(14, 30, 0, 0),
+    //           child: Text(
+    //             'Favorites',
+    //             style: Theme.of(context).textTheme.displayMedium,
+    //           ),
+    //         ),
+    //         const SizedBox(
+    //           height: 14,
+    //         ),
+    //         Expanded(
+    //           child: ListView.builder(
+    //             itemBuilder: (context, index) {
+    //               return ListTile(
+    //                 title: Text('Quote #$index'),
+    //                 subtitle: Text('Author #$index'),
+    //                 trailing: IconButton(
+    //                   icon: Icon(Icons.favorite_outline),
+    //                   onPressed: () {},
+    //                 ),
+    //                 style: Theme.of(context).listTileTheme.style,
+    //               );
+    //             },
+    //             itemCount: 5,
+    //           ),
+    //         ),
+    //       ],
+    //     ));
+  //* UNCOMMENT FOR LATER
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.only(top: 14),
-            child: IconButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                icon: const Icon(Icons.arrow_back, size: 34,)),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            // actions: [
+            //   IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back))
+            // ],
+            //title: Text('Favorites'),
+            automaticallyImplyLeading: false,
+            expandedHeight: 150,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                  alignment: Alignment.bottomLeft,
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+              title: Text('Favorites',textAlign: TextAlign.left,),
+            ),
+            //titleTextStyle: Theme.of(context).textTheme.headlineSmall,
           ),
-          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-          titleTextStyle: Theme.of(context).appBarTheme.titleTextStyle,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(14, 30, 0, 0),
-              child: Text(
-                'Favorites',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return ListTile(
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                                  return ListTile(
                     title: Text('Quote #$index'),
                     subtitle: Text('Author #$index'),
                     trailing: IconButton(
@@ -45,11 +91,32 @@ class Favorites extends StatelessWidget {
                     ),
                     style: Theme.of(context).listTileTheme.style,
                   );
-                },
-                itemCount: 5,
-              ),
+              },
+              childCount: 20,
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
+
+  //try new
+  // List<Widget> headerSliverBuilder(context, innerBoxIsScrolled) {
+  //   return [SliverOverlapAbsorber(handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+  //   sliver: SliverAppBar(
+  //     backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+  //     expandedHeight: 150,
+  //     pinned: true,
+  //     toolbarHeight: kToolbarHeight,
+  //   ),)];
+  // }
+
+  // Widget body() {
+  //   return Container();
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return NestedScrollView(headerSliverBuilder: headerSliverBuilder, body: body);
+  // }
 }
